d72fcb5694963db61c9d0548dbc844547dba0eb4
Thiago Macedo <thialmaceod@gmail.com>
2023-08-31 11:55:07 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="skynet_ml",
+    version="0.0.1",
+    author="Thiago Macedo",
+    author_email="thialmacedo@gmail.com",
+    description="A hand made machine learning library from scratch, by Thiago Macedo",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
