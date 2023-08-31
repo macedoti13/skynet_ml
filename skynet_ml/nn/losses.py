22b7d5848c79b59787ad35c8d69edaf60cd6f7fc
Thiago Macedo <thialmaceod@gmail.com>
2023-08-31 15:21:47 -0300

    new version 0.0.4: binary cross-entropy loss

--- skynet_ml/nn/losses.py
+++ skynet_ml/nn/losses.py
@@ -7,6 +7,8 @@ def get_loss_function(loss: str) -> Callable[..., Any]:
     """
     if loss == "mse":
         return mse
+    elif loss == "bce":
+        return binary_cross_entropy
 
 
 def get_loss_derivative(loss: str) -> Callable[..., Any]:
@@ -15,6 +17,8 @@ def get_loss_derivative(loss: str) -> Callable[..., Any]:
     """
     if loss == "mse":
         return mse_derivative
+    elif loss == "bce":
+        return binary_crossentropy_derivative
     
     
 def mse(yhat: float, y: float) -> float:
@@ -37,3 +41,32 @@ def mse_derivative(yhat: float, y: float) -> float:
         y (float): True Label.
     """    
     return yhat - y
+
+
+def binary_cross_entropy(yhat: float, y: float) -> float:
+    """
+    Binary Cross-Entropy Loss function. 
+
+    Args:
+        yhat (float): Prediction.
+        y (float): True label.
+
+    Returns:
+        float: Loss value. Folows the formula for the BCE Loss.
+    """    
+    epsilon = 1e-15  # To avoid log(0)
+    yhat = np.clip(yhat, epsilon, 1 - epsilon)
+    return np.mean(-(y * np.log(yhat) + (1 - y) * np.log(1 - yhat)))
+
+
+def binary_crossentropy_derivative(yhat: float, y: float) -> float:
+    """
+    Derivative of the Binary Cross Entropy Loss Function.
+
+    Args:
+        yhat (float): Prediction.
+        y (float): True Label.
+    """
+    epsilon = 1e-15  # added to prevent division by 0 error
+    yhat = np.clip(yhat, epsilon, 1 - epsilon)
+    return (yhat - y) / (yhat * (1 - yhat))
