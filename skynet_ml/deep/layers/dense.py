softmax fixed

skynet_ml/deep/layers/dense.py
@@ -92,7 +92,10 @@ class Dense:
         assert d_output.shape == self.a.shape, "d_output shape mismatch. Expected shape: {} but got: {}.".format(self.a.shape, d_output.shape)
 
         # calculates the delta for this layer: 
-        self.delta = np.multiply(d_output, self.d_activation(self.z))
+        if self.activation == "softmax":
+            self.delta = d_output
+        else:
+            self.delta = np.multiply(d_output, self.d_activation(self.z))
         
         # computes the gradient of the loss w.r.t weights
         self.d_weights = np.dot(self.delta, self.input_vector.T)

skynet_ml/deep/losses/losses.py
@@ -70,12 +70,10 @@ def categorical_cross_entropy(yhat: float, y: float) -> float:
 
 def d_categorical_cross_entropy(yhat: float, y: float) -> float:
     """
-    Derivative of the Categorical Cross Entropy Loss Function
+    Derivative of the Categorical Cross Entropy Loss Function when combined with softmax.
 
     Args:
         yhat (float): Prediction.
         y (float): True Label.
     """    
-    epsilon = 1e-15  # prevents division by 0
-    yhat = np.clip(yhat, epsilon, 1 - epsilon)
-    return -y / yhat
+    return yhat - y