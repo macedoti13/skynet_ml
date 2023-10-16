a7addb3529038c37e1734d5267e885ccd12cd30f
Thiago Macedo <thialmaceod@gmail.com>
2023-10-15 23:24:07 -0300

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="skynet_ml",
-    version="1.2.0.1",
+    version="1.2.0.2",
     author="Thiago Macedo",
     author_email="thialmacedo@gmail.com",
     description="A hand made machine learning library from scratch, by Thiago Macedo",
